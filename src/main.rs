42a348195fc244008540285a6177958a664fdcc5
Daniel Wong <daniel.wong@dfinity.org>
2025-11-25 13:44:38 +0100

    Mainly, "wired in" flags for downloading signed reply. Plus, misc.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use base64::Engine;
 use clap::Parser;
 use ic_agent::{Agent, export::Principal};
 use ic_certification::{Certificate, HashTree, LookupResult, SubtreeLookupResult};
-use std::{collections::HashSet, fs, io::Write, time::Duration};
+use std::{
+    collections::HashSet,
+    fs,
+    io::{Read, Write, stdin},
+    time::Duration,
+};
 
 #[derive(clap::Parser)]
 struct Args {
@@ -17,21 +23,22 @@ impl Args {
 
 #[derive(clap::Subcommand)]
 enum Subcommand {
-    Download(Download),
-    LoadFromFile(LoadFromFile),
+    DownloadSignedReply(DownloadSignedReply),
+    LoadSignedReplyFromFile(LoadSignedReplyFromFile),
 }
 
 impl Subcommand {
     async fn execute(self) {
+        #[rustfmt::skip] // Keep alignment, because it strongly triggers the human brain.
         match self {
-            Self::Download(ok)     => ok.execute().await,
-            Self::LoadFromFile(ok) => ok.execute(),
+            Self::DownloadSignedReply     (ok) => ok.execute().await,
+            Self::LoadSignedReplyFromFile (ok) => ok.execute(),
         };
     }
 }
 
 #[derive(clap::Parser)]
-struct Download {
+struct DownloadSignedReply {
     #[arg(long)]
     callee: Principal,
 
@@ -42,22 +49,23 @@ struct Download {
     arg_path: String,
 }
 
-impl Download {
-    async fn execute(&self) {
+impl DownloadSignedReply {
+    async fn execute(self) {
         let Self {
             callee,
             method,
             arg_path,
         } = self;
 
-        /*
         let arg = if arg_path == "-" {
+            let mut arg = vec![];
+            stdin().read_to_end(&mut arg).unwrap();
+            arg
         } else {
-            fs::read(arg_path).unwrap();
+            fs::read(arg_path).unwrap()
         };
-        */
 
-        let signed_proposal = download_signed_proposal(/* callee, method, arg*/).await;
+        let signed_proposal = download_signed_proposal(callee, &method, arg).await;
         let signed_proposal = serde_cbor::to_vec(&signed_proposal).unwrap();
 
         let stdout = std::io::stdout();
@@ -70,21 +78,23 @@ impl Download {
 }
 
 #[derive(clap::Parser)]
-struct LoadFromFile {
+struct LoadSignedReplyFromFile {
     #[arg(long)]
     signed_reply_path: String,
 }
 
-impl LoadFromFile {
-    fn execute(&self) {
-        let Self {
-            signed_reply_path,
-        } = self;
+impl LoadSignedReplyFromFile {
+    fn execute(self) {
+        let Self { signed_reply_path } = self;
 
         let content = fs::read(&signed_reply_path).unwrap();
         let certificate = serde_cbor::from_slice::<Certificate>(&content).unwrap();
         let reply = verify_signed_proposal(certificate);
-        println!("{reply:?}");
+        let reply = reply
+            .into_iter()
+            .map(|element| format!("{:02X}", element))
+            .collect::<String>();
+        println!("{reply}");
     }
 }
 
@@ -93,17 +103,19 @@ async fn main() {
     Args::parse().execute().await;
 }
 
-async fn download_signed_proposal() -> Certificate {
+async fn download_signed_proposal(
+    callee: Principal,
+    method_name: &str,
+    arg: Vec<u8>,
+) -> Certificate {
     let agent = Agent::builder()
         .with_url("https://ic0.app")
         .build()
         .unwrap();
 
-    let governance_principal = Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap();
-
     let (_reply, certificate): (Vec<u8>, Certificate) = agent
-        .update(&governance_principal, "get_build_metadata")
-        .with_arg(candid::encode_args(()).unwrap())
+        .update(&callee, method_name)
+        .with_arg(arg)
         .call()
         .and_wait()
         .await
@@ -112,7 +124,7 @@ async fn download_signed_proposal() -> Certificate {
     certificate
 }
 
-fn verify_signed_proposal(certificate: Certificate) -> (String,) {
+fn verify_signed_proposal(certificate: Certificate) -> Vec<u8> {
     let a_very_long_time = Duration::from_secs(365_250_000 * 24 * 60 * 60);
 
     let agent = Agent::builder()
@@ -125,10 +137,20 @@ fn verify_signed_proposal(certificate: Certificate) -> (String,) {
     const IC_ROOT_PUBLIC_KEY_BASE64: &str = r#"MIGCMB0GDSsGAQQBgtx8BQMBAgEGDCsGAQQBgtx8BQMCAQNhAIFMDm7HH6tYOwi9gTc8JVw8NxsuhIY8mKTx4It0I10U+12cDNVG2WhfkToMCyzFNBWDv0tDkuRn25bWW5u0y3FxEvhHLg1aTRRQX/10hLASkQkcX4e5iINGP5gJGguqrg=="#;
     // This is copied from rs/embedders.
     const IC_ROOT_KEY: &[u8; 133] = b"\x30\x81\x82\x30\x1d\x06\x0d\x2b\x06\x01\x04\x01\x82\xdc\x7c\x05\x03\x01\x02\x01\x06\x0c\x2b\x06\x01\x04\x01\x82\xdc\x7c\x05\x03\x02\x01\x03\x61\x00\x81\x4c\x0e\x6e\xc7\x1f\xab\x58\x3b\x08\xbd\x81\x37\x3c\x25\x5c\x3c\x37\x1b\x2e\x84\x86\x3c\x98\xa4\xf1\xe0\x8b\x74\x23\x5d\x14\xfb\x5d\x9c\x0c\xd5\x46\xd9\x68\x5f\x91\x3a\x0c\x0b\x2c\xc5\x34\x15\x83\xbf\x4b\x43\x92\xe4\x67\xdb\x96\xd6\x5b\x9b\xb4\xcb\x71\x71\x12\xf8\x47\x2e\x0d\x5a\x4d\x14\x50\x5f\xfd\x74\x84\xb0\x12\x91\x09\x1c\x5f\x87\xb9\x88\x83\x46\x3f\x98\x09\x1a\x0b\xaa\xae";
-    assert_eq!(&base64::decode(IC_ROOT_PUBLIC_KEY_BASE64).unwrap(), IC_ROOT_KEY);
-    assert_eq!(agent.read_root_key(), base64::decode(IC_ROOT_PUBLIC_KEY_BASE64).unwrap());
+    assert_eq!(
+        &base64::engine::general_purpose::STANDARD
+            .decode(IC_ROOT_PUBLIC_KEY_BASE64)
+            .unwrap(),
+        IC_ROOT_KEY
+    );
+    assert_eq!(
+        agent.read_root_key(),
+        base64::engine::general_purpose::STANDARD
+            .decode(IC_ROOT_PUBLIC_KEY_BASE64)
+            .unwrap()
+    );
 
-    let corrupt_root_key = {
+    let _corrupt_root_key = {
         let mut result = agent.read_root_key();
         let last_index = result.len() - 1;
         result[last_index] = 0;
@@ -143,13 +165,13 @@ fn verify_signed_proposal(certificate: Certificate) -> (String,) {
         .unwrap_or_else(|err| {
             panic!("INPUT DOES NOT SEEM TO BE A GENUINE RESPONSE FROM THE CANISTER: {err:?}");
         });
-    println!();
-    println!(
+    eprintln!();
+    eprintln!(
         "👍 Certificate looks good. That is, we seem to have genuine data from\n\
          the ICP (presumably, a response from the Governance canister, but this\n\
          part has not been verified YET).",
     );
-    println!();
+    eprintln!();
 
     /*
     let paths = certificate
@@ -172,7 +194,7 @@ fn verify_signed_proposal(certificate: Certificate) -> (String,) {
     let request_status = RequestStatus::try_from_tree(certificate.tree).unwrap();
     assert_eq!(&request_status.status, "replied");
 
-    candid::decode_args::<(String,)>(&request_status.reply).unwrap()
+    request_status.reply
 }
 
 #[derive(Debug, PartialEq, Eq, Default)] // DO NOT MERGE
